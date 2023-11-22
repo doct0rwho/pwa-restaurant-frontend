ae5e547811c2c12d57d60a2724c89e592d7b6051
doct0rwho <45739838+doct0rwho@users.noreply.github.com>
2023-11-22 15:35:47 +0200

    123

--- pwa-app/src/App.vue
+++ pwa-app/src/App.vue
@@ -3,9 +3,18 @@
     <!-- Your existing content -->
     <div v-if="isMobileDevice">
      <div>
-    <Button label="Click"></Button>
-    <Button icon="pi pi-check" label="Accept"></Button>
-    <Button icon="pi pi-times" class="p-button-rounded p-button-danger"></Button>
+      <Button class="transparent-button" @click="visible = true">
+        <i class="pi pi-bars"></i>
+      </Button>
+      <div class="card flex justify-content-center">
+    <Sidebar v-model:visible="visible" class="custom-sidebar" >
+        <!-- <Button class="transparent-button" @click="visible = false">
+          <i class="pi pi-times"></i>
+        </Button> -->
+    </Sidebar>
+    
+</div>
+    
   </div>
     </div>
     <div v-else>
@@ -17,12 +26,14 @@
 <script>
 
 
+
 export default {
   name: 'App',
   
   data() {
     return {
-      isMobileDevice: false
+      isMobileDevice: false,
+      visible: false
     };
   },
   created() {
@@ -36,7 +47,42 @@ export default {
         alert('This application is only available on mobile devices.');
       }
     }
+
   }
 };
 </script>
 
+<style scoped>
+#app {
+  position: relative;
+}
+
+.transparent-button {
+  background: none;
+  border: none;
+  color: black;
+  position: absolute;
+  top: 0;
+  right: 0;
+}
+.transparent-button:focus {
+  outline: none; /* Optional: Remove focus outline */
+  box-shadow: none; /* Remove the button shadow on focus */
+}
+.p-sidebar .p-sidebar-header .p-sidebar-close, .p-sidebar .p-sidebar-header .p-sidebar-icon {
+  display: none !important;
+}
+@layer primevue {
+  /* Hide the close button and icon in the sidebar header */
+  .p-sidebar .p-sidebar-header .p-sidebar-close,
+  .p-sidebar .p-sidebar-header .p-sidebar-icon {
+    display: none !important;
+  }
+
+  /* Add any additional styles you need */
+  .p-sidebar {
+    /* Your custom styles for the sidebar */
+  }
+}
+
+</style>
--- pwa-app/src/main.js
+++ pwa-app/src/main.js
@@ -6,10 +6,19 @@ import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primevue/resources/primevue.min.css'; 
 import 'primeicons/primeicons.css'; 
 import Button from 'primevue/button';
+import Menu from 'primevue/menu';
+import VueSidebarMenu from 'vue-sidebar-menu'
+
+import Sidebar from 'primevue/sidebar';
+
+import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
 
 const app = createApp(App);
 
 app.use(PrimeVue);
 app.use(router);
+app.use(VueSidebarMenu);
 app.component('Button', Button);
+app.component('Menu', Menu);
+app.component('Sidebar', Sidebar);
 app.mount('#app');
