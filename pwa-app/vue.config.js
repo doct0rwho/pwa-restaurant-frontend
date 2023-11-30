882e8759f1d5e5c16c0569aaeaab8b8a976e4253
doct0rwho <45739838+doct0rwho@users.noreply.github.com>
2023-11-30 18:49:34 +0200

    123

--- pwa-app/vue.config.js
+++ pwa-app/vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  // other configurations...
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'Your New App Name';
+      return args;
+    });
+  },
+  pwa: {
+    name: 'Resturant Management System', // <---- this is PWA name
+  }
+};
