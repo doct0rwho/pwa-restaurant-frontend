a947888cb2a8b4be0ab3b355b9af43442f1defa5
doct0rwho <45739838+doct0rwho@users.noreply.github.com>
2023-12-12 01:59:17 +0200

    1

--- pwa-app/src/views/Menu.vue
+++ pwa-app/src/views/Menu.vue
@@ -165,7 +165,7 @@ import { ref } from "vue";
 import { toast } from 'vue3-toastify';
 import 'vue3-toastify/dist/index.css';
 import io from 'socket.io-client';
-const socket = io("localhost:4000");
+const socket = io("https://diploma-lya6.onrender.com");
 socket.on('connect', () => {
   console.log('connected');
   offline.value = false;
